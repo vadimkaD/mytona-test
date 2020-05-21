d5938aba8270365813bba01a20539b53f4674c95
vadimkaD <fantik217@gmail.com>
2020-05-21 14:40:30 +0300

    feature: add build to repo

---
+++ build/precache-manifest.6bd580e0f2a6a3e1bf81681be84f5ca1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e78b41e41f2c6a3f14b3539dc06daaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f320dbd3e4791a06620",
+    "url": "/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "5f2c25afb59775fa84bb",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8f320dbd3e4791a06620",
+    "url": "/static/js/2.844771c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.844771c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2c25afb59775fa84bb",
+    "url": "/static/js/main.f21cd1b0.chunk.js"
+  },
+  {
+    "revision": "c5b7b6097c44addfe079",
+    "url": "/static/js/runtime-main.1cf82775.js"
+  },
+  {
+    "revision": "2dfa100b2df92722a71afd96111cb591",
+    "url": "/static/media/cb-girl-1.2dfa100b.png"
+  },
+  {
+    "revision": "4fe3a39eaaca5d6fed5ffb8987bf6d00",
+    "url": "/static/media/cb-girl-2.4fe3a39e.jpg"
+  },
+  {
+    "revision": "92a4360077eb5c8fbbbba537f6a6da88",
+    "url": "/static/media/cb-girl-3.92a43600.jpg"
+  },
+  {
+    "revision": "48ee1f89ec1ed7c4e11349793e05ecf4",
+    "url": "/static/media/cb-girl-4.48ee1f89.jpg"
+  },
+  {
+    "revision": "0ebd3ce40790eff7b39406f58b65770e",
+    "url": "/static/media/cb-girl-5.0ebd3ce4.jpg"
+  },
+  {
+    "revision": "532d8e5c11805d76646ea055415fbf0d",
+    "url": "/static/media/cb-girl-6.532d8e5c.png"
+  },
+  {
+    "revision": "95fbdb1a5d2f41830ecec960483fbd8b",
+    "url": "/static/media/homepage.95fbdb1a.jpg"
+  }
+]);
